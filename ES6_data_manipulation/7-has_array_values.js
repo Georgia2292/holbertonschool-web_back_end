e0b02cb6d324f4224ad177750a8a86125b97e774
Georgia2292 <georgelina2292@gmail.com>
2024-04-18 04:55:23 +0000

    More set data structure

--- ES6_data_manipulation/7-has_array_values.js
+++ ES6_data_manipulation/7-has_array_values.js
@@ -1,5 +1,6 @@
-import hasValuesFromArray from "./7-has_array_values.js";
+function hasValuesFromArray(set, array) {
+  const check = array.every((item) => set.has(item));
+  return check;
+}
 
-console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1]));
-console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [10]));
-console.log(hasValuesFromArray(new Set([1, 2, 3, 4, 5]), [1, 10]));
+export default hasValuesFromArray;
